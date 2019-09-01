b198504193b87b52bc54166b0bf491c8b69dccf9
Sub Estacion <subestacion@paseolagaleria.com>
2019-08-31 21:01:37 -0400

    Se agrega a repositorio

---
+++ principal.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rutinas electricas</title>
+</head>
+<body>
+    <header>
+        <h1>Menu Principal</h1>
+    </header>
+    <div>
+        <button>Nueva Tarea</button>
+        <button>Historias de tareas</button>
+        <button>Tareas Pendientes</button>
+        <button>Reportes de horas</button>
+    </div>
+</body>
+</html>
+
