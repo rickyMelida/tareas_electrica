a750d1fff19afc801009e73e12b3579ee133c798
Ricardo Mélida <ricardomelida92@gmail.com>
2019-09-02 09:17:21 -0400

    Se agrega conexion a BD

--- validaciones/conexionBD.php
+++ validaciones/conexionBD.php
@@ -1,16 +1,14 @@
 <?php
-    $servername = "db4free.net:3306";
-    $database = "radio_panambi";
-    $username = "ricky_melida";
-    $password = "5181789781Ri-";
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    //Con esto podemos seleccionar una base de datos pero debemos de eliminar de la conexion dicha bd
-    //$bd_seleccionada = mysqli_ select_db ($nombreConexión, $nombreBaseDatos);
-    // Check connection
-    if (!$conn) {
-        die("No se pudo conectar a la BD: " . mysqli_connect_error());
+    class conectar {
+        private $servidor = "db4free.net:3306";
+        private $database = "radio_panambi";
+        private $username = "ricky_melida";
+        private $password = "5181789781Ri-";
+
+        public function conexion() {
+            $conexion = mysqli_connect($servername, $username, $password, $database);
+
+            return $conexion;
+        }
     }
-    //echo "Conexion Establecida";
-    //mysqli_close($conn);
 ?>
