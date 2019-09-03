c53258924cd8dcb2ac68618db1f88a13ee139f3b
Ricardo Mélida <ricardomelida92@gmail.com>
2019-09-03 06:51:37 -0400

    Se cambia BD por una del servidor local

--- validaciones/conexionBD.php
+++ validaciones/conexionBD.php
@@ -1,8 +1,8 @@
 <?php
     class conectar {
-        private $servidor = "db4free.net:3306";
-        private $database = "radio_panambi";
-        private $username = "ricky_melida";
+        private $servidor = "localhost";
+        private $database = "tareas_electrica";
+        private $username = "root";
         private $password = "5181789781Ri-";
 
         public function conexion() {
