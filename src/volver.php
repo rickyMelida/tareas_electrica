0f9a8b688e43fb8611eba8de62e16d64c5f55858
Ricardo Mélida <ricardomelida92@gmail.com>
2019-09-07 09:39:07 -0400

    Se agrega solo estilos bootstrap a la advertencia

--- src/volver.php
+++ src/volver.php
@@ -6,35 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Error..</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <style>
-        body{
-            /*background: #708090;*/
-            color: #fff;
-            background: rgba(255, 0, 0, 0.8);
-            font-size: 1.2em;
-        }
-        
-    </style>
-</head>
 <body>
     <div class="container">
-        <div class="row">   
-            <div class="col-lg-2 col-md-12 pl-5">
-                <img src="../iconos/error2.png" width="250" height="250" class="float-right"> 
-            </div>
-
-            <div class="col-lg-10 col-md-12 mt-5">
-                <h1>Favor iniciar sesion.</h1>
-                <p>   
-                    Debes iniciar sesión para poder visualizar el contenido de esta página.
-                </p>
-                <p>
-                    Presione <b>volver</b> para volver al inicio.</li>
-                </p>
-                <p>
-                    <a href="../index.php" class="btn btn-link">Volver..</a>
-                </p>
-            </div>
+        <div class="alert alert-warning" role="alert">
+            <h4 class="alert-heading">Advertencia!</h4>
+            <p>Favor iniciar sesion.<br>Debes iniciar sesión para poder visualizar el contenido de esta página.</p>
+            <hr>
+            <p class="mb-0">Presione <a href="../index.php" class="text-dark">volver..</a> para volver al inicio.</p>
         </div>
     </div>
 </body>
