fda0f3aa74472315c9d634b456e0fea8b7776863
Ricardo Mélida <ricardomelida92@gmail.com>
2019-09-07 12:59:34 -0400

    Se agrega validaciones de radio button

--- js/design.js
+++ js/design.js
@@ -5,3 +5,18 @@ function agregar() {
 function pendientes() {
     window.open('pendientes.php', '_self');
 }
+
+function estado_trabajo() {
+    for(var i=0; document.formulario.estado.length; i++) {
+        if(document.formulario.estado[i].checked) 
+            break;
+    }
+
+    var estado = document.formulario.estado[i].value;
+
+    if(estado = "pendiente") {
+        document.getElementById('tecnico_1').disabled=true;
+    }else if(estado == "finalizado"){
+        document.getElementById('tecnico_1').disabled=false;
+    }
+}
