50c625adfe2a45cc77d4606096c6d933367a2f76
Ricardo Mélida <ricardomelida92@gmail.com>
2019-09-09 11:31:09 -0400

    Se agregan respuestas de formulario agregar

--- validaciones/formulario.php
+++ validaciones/formulario.php
@@ -1,4 +1,16 @@
 <?php
+    $tipo_tr = $_POST['t_trabajo'];
+    $estado_tr = $_POST['estado'];
+
+    $tecnico_1 = $_POST['tecnico_1'];
+    $tecnico_2 = $_POST['tecnico_2'];
+    $tecnico_3 = $_POST['tecnico_3'];
+
+    $turno = $_POST['turno'];
     
+    echo 'el tipo de trabajo es '. $tipo_tr."<br>";
+    echo 'El estado es ' . $estado_tr;
+
+
 
 ?>
