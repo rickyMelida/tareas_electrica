99a6cfa34fc8b1bd3bd7bf32de3adfa8275d8f97
Sub Estacion <subestacion@paseolagaleria.com>
2019-09-12 13:16:27 -0400

    Se condiciona acceso a base de datos en caso de falta de conexion al servidor

--- validaciones/validar_usuario.php
+++ validaciones/validar_usuario.php
@@ -2,33 +2,35 @@
     require_once "conexionBD.php";
     session_start();
 
-    $obj = new conectar();
-    $con = $obj->conexion();
-
-    $usuario = $_POST['usuario'];
-    $pass = $_POST['pass'];
-
-    mysqli_set_charset($con,'utf8');
-    $sql = "SELECT * from usuarios where usuario= '$usuario' and pass='$pass'";
-
-    $resultado = mysqli_query($con, $sql);
-
-    $filas = mysqli_num_rows($resultado);
-
     if (!$con) {
-        die("No se pudo conectar a la BD: " . mysqli_connect_error());
-    }
-
-    if($filas > 0) {
-        $_SESSION['usuario'] = $usuario;
-        header('Location: ../src/principal.php');
-
+        header('Location: ../src/errorDB.php');
     }else {
-        echo "<script> alert('Contraseña o usuario incorrecto');</script>";
-        header('Location: ../index.php');
 
+        $obj = new conectar();
+        $con = $obj->conexion();
+
+        $usuario = $_POST['usuario'];
+        $pass = $_POST['pass'];
+
+        mysqli_set_charset($con,'utf8');
+        $sql = "SELECT * from usuarios where usuario= '$usuario' and pass='$pass'";
+
+        $resultado = mysqli_query($con, $sql);
+
+        $filas = mysqli_num_rows($resultado);
+
+        
+
+        if($filas > 0) {
+            $_SESSION['usuario'] = $usuario;
+            header('Location: ../src/principal.php');
+
+        }else {
+            echo "<script> alert('Contraseña o usuario incorrecto');</script>";
+            header('Location: ../index.php');
+
+        }
     }
-    
 
 
 
