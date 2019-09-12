81d6967a234eeb154f8e6b4594a9eac65620eba7
Sub Estacion <subestacion@paseolagaleria.com>
2019-09-12 13:17:24 -0400

    Se agrega pagina en el caso de que no se pueda conectar a la base de datos

---
+++ src/errorDB.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Error..</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<body>
+    <div class="container">
+        <div class="alert alert-warning" role="alert">
+            <h4 class="alert-heading">Advertencia!</h4>
+            <p>No se pudo conectar a la Base de Datos.<br>Favor contactar con el administrador de servicio o tecnico de sistemas.</p>
+            <hr>
+            <p class="mb-0">Presione <a href="../index.php" class="text-dark">volver..</a> para volver al inicio.</p>
+        </div>
+    </div>
+</body>
+</html>
