84f810588b048d37386244b2cc12d92fd3b67bd0
Ricardo Mélida <ricardomelida92@gmail.com>
2019-09-30 11:44:18 -0400

    Se realizan los calculos de las horas hombres desde el frontend

--- validaciones/formulario.php
+++ validaciones/formulario.php
@@ -21,60 +21,19 @@
     $hora_inicial = $_POST['h_inicial'];
     $hora_final = $_POST['h_final'];
 
+    $horas_hombre = $_POST['h_hombre'];
+
     $descripcion = $_POST['descripcion'];
 
     //Guardamos la fecha que extraemos del sistema
-    $fecha = date("Y")."-".date("m")."-".date("d");
+    $fecha = date("d")."-".date("m")."-".date("Y");
 
     
 
-
-    if(validarHora($hora_inicial)) {
-        $h_ini_val = $hora_inicial;
-    }else {
-        echo "<script>alert('Hora no valido');
-        Location.href = '../src/agregar.php';
-        </script>";
-    }
-
-    if(validarHora($hora_final)) {
-        $h_fin_val = $hora_final;
-    }else {
-        echo "<script>alert('Hora no valido');
-            Location.href = '../src/agregar.php';
-        </script>";
-    }
-
-
-	$hours_i = array();
-	$hours_f = array();
-
-	$hours_i = horas($h_ini_val);
-	$hours_f = horas($h_fin_val);
-
-
-	$hrs_i = $hours_i[0];
-	$min_i = $hours_i[1];
-
-	$hrs_f = $hours_f[0];
-	$min_f = $hours_f[1];
-
-	$hrs_h = $hrs_f - $hrs_i;
-	$min_h = $min_f - $min_i;
-
-    $horas_hombre = $hrs_h.":".$min_h;
-    $datos = array($tipo_tr, $descripcion, $fecha, $hora_inicial, $hora_final, $horas_hombre, $estado_tr, $turno, $tecnico_1);
-
-    /*
-    $obj = new metodos();
-    if($obj->agregar($datos) == 1) {
-        echo "<hr><br>Se agrego los datos correctamente";
-    }else {
-        echo "<hr><br>No se pudo agregar los datos";
-
-    }*/
     echo "Se inicio a las " . $hora_inicial."<br>";
     echo "Termino a las " . $hora_final."<br>";
 
-    echo "<br>Texto de prueba y la hora es ".$horas_hombre;
+    echo "<br>Texto de prueba y la hora es ".$horas_hombre."<br>";
+
+    echo "En la fecha ". $fecha;
 ?>
