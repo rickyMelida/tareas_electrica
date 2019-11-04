69441dabe6956c51e053e84450588c7b7c644a54
Sub Estacion <subestacion@paseolagaleria.com>
2019-11-04 19:20:47 -0300

    Se agrega icono de pestaña en cada pagina

--- src/errorDB.php
+++ src/errorDB.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Error..</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../iconos/electrico.ico" type="image/x-icon">
 <body>
     <div class="container">
         <div class="alert alert-warning" role="alert">
