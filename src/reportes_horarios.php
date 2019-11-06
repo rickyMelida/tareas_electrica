4138e4fa5d0481066b7bcf15aa910894c6edee0f
Sub Estacion <subestacion@paseolagaleria.com>
2019-11-06 18:02:35 -0300

    Se agrega archivo de modificacion de horario de tecnico

--- src/reportes_horarios.php
+++ src/reportes_horarios.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,38 +9,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Actualizar Tecnico</title>
+    <title>Horarios</title>
     <link rel="shortcut icon" href="../iconos/electrico.ico" type="image/x-icon">
 </head>
-<body>
+<body class="bg-light">
     <div class="container border border-primary">
         <header class="text-center bg-primary p-4">   
-            <a href="horario_tecnicos.php" class="float-left m-3 btn btn-outline-dark">Volver</a>
-            <h1 class=" d-inline">Actualizar</h1>
+            <a href="reportes.php" class="float-left m-3 btn btn-outline-dark">Volver</a>
+            <h1 class=" d-inline">Tecnicos</h1>
         </header>
         <div class="row">
-            <form action="" method="" class="col-md-8 m-auto p-3">
-                <div class="form-group">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" class="form-control" id="nombre" >
-                </div>
-                <div class="form-group">
-                    <label for="cargo">Cargo</label>
-                    <select name="cargo" id="cargo" class="form-control">
-                        <option value="junior">Junior</option>
-                        <option value="senior">Senior</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="turno">Turno</label>
-                    <select name="cargo" id="cargo" class="form-control">
-                        <option value="mañana">Mañana</option>
-                        <option value="tarde">Tarde</option>
-                        <option value="noche">Noche</option>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary">Actualizar</button>
-            </form>
+            <div class="col-md-8 m-auto p-3">
+                
+            </div>
         </div>
     </div>
 
@@ -44,6 +29,5 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
 </body>
 </html>
