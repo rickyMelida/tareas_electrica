b77a68d3e9735c90d4bedacfbbbe52a1558c8326
Ricardo Mélida <ricardomelida92@gmail.com>
2019-12-02 11:20:01 -0300

    Se agrega ejemplo de grafica en barra

--- src/reportes_horarios.php
+++ src/reportes_horarios.php
@@ -6,6 +6,7 @@
     //Objetivo de las horas hombre
     $objetivo = "14:00:00";
 
+    $var_session = $_COOKIE['admin'];
     $obj = new metodos();
 
     //Seleccion de todos los tecnicos
@@ -238,11 +239,66 @@
 
                 chart.draw(data, options);
             }
+            /*------------------------------------------------------------------*/
+
+
+
+            google.charts.load('current', {packages: ['corechart', 'bar']});
+            google.charts.setOnLoadCallback(drawBasic);
+
+            function drawBasic() {
+
+                var data = new google.visualization.DataTable();
+                data.addColumn('timeofday', 'Time of Day');
+                data.addColumn('number', 'Motivation Level');
+
+                data.addRows([
+                    [{v: [8, 0, 0], f: 'Camilo Barreto'}, 1],
+                    [{v: [9, 0, 0], f: 'Ricardo Melida'}, 2],
+                    [{v: [10, 0, 0], f:'10 am'}, 3],
+                    [{v: [11, 0, 0], f: '11 am'}, 4],
+                    [{v: [12, 0, 0], f: '12 pm'}, 5],
+                    [{v: [13, 0, 0], f: '1 pm'}, 6],
+                    [{v: [14, 0, 0], f: '2 pm'}, 7],
+                    [{v: [15, 0, 0], f: '3 pm'}, 8],
+                    [{v: [16, 0, 0], f: '4 pm'}, 9],
+                    [{v: [17, 0, 0], f: '5 pm'}, 10],
+
+                ]);
+
+                var options = {
+                    title: 'Total de horas por tecnico',
+                    hAxis: {
+                        title: 'Time of Day',
+                        format: 'h:mm a',
+                        viewWindow: {
+                            min: [7, 30, 0],
+                            max: [17, 30, 0]
+                        }
+                    },
+                    vAxis: {
+                        title: 'Horas Hombre'
+                    }
+                };
+
+                var chart = new google.visualization.ColumnChart(
+                document.getElementById('barra'));
+
+                chart.draw(data, options);
+            }
 
         </script>
+        <!-- Grafica en torta por sector -->
         <div class="row">
             <div class="col-md-12 m-auto p-3 ">
                 <div id="grafica" class="m-auto w-100"></div>
+            </div>  
+        </div>
+
+        <!-- Grafica en barra por tecnico -->
+        <div class="row">
+            <div class="col-md-12 m-auto p-3 ">
+                <div id="barra" class="m-auto w-100"></div>
             </div>  
         </div>
     </div>
