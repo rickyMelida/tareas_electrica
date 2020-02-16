821931ca07d5a2430cdb2f2a616f79a79cf6dced
Sub Estacion <subestacion@paseolagaleria.com>
2020-02-16 19:47:13 -0300

    Se agregan estilos para el login en el diseño para movil

--- includes/login.php
+++ includes/login.php
@@ -1,17 +1,18 @@
 <body>
     <div class="container">
-        <h1>Planilla de Tareas - Eléctrica</h1>
-            <form method="post" action="<?php echo APP.'validar_usuario.php';?>">
-                <div class="form-group">
-                    <label>Usuario</label>
-                    <input type="text" name="usuario" placeholder="Usuario">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="pass" placeholder="Password">
-                </div>
-                    <button type="submit" class="btn">Ingresar</button>
-            </form>
+        <form method="post" action="<?php echo APP.'validar_usuario.php';?>">
+            <h1>Eléctrica PLG</h1>
+            <img src="./assets/iconos/electricista.png" alt="sale">
+            <div class="form-group">
+                <label>Usuario</label>
+                <input type="text" name="usuario" placeholder="Usuario">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="pass" placeholder="Password">
+            </div>
+                <button type="submit" class="btn">Ingresar</button>
+        </form>
     </div>
 </body>
 <script src="<?php echo JS.'jquery-3.4.1.js';?>" ></script>
